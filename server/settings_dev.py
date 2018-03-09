fbc3ad22f45017dc36ca02dd9472876c3c3a171a
Edouard Richard <edou4rd@gmail.com>
2018-03-09 10:38:53 +0100

    postgres on dev

--- server/settings_dev.py
+++ server/settings_dev.py
@@ -1,4 +1,17 @@
 from settings import *
+import os
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 MIDDLEWARE.append('middleware.dev_cors_middleware')
+
+if os.environ.get('POSTGRES', False):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'bias',
+            'USER': 'postgres',
+            'PASSWORD': 'mysecretpassword',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
